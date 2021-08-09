2a41d3be1e6402654848e9082d1ae9574f60cf1b
Kevin Rios <rios.kevin.12@gmail.com>
2021-08-09 16:25:38 +0200

    ex02

---
+++ module00/ex02/Account.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/08 21:20:31 by krios-fu          #+#    #+#             */
+/*   Updated: 2021/08/08 21:38:27 by krios-fu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+
+
+Account::Account (int initial_deposit)
+{
+	(void) initial_deposit;
+	
+}
+
+Account::~Account (void)
+{
+	
+}
+
+
+/* GETTERS */
+
+int Account::getNbAccounts(void)
+{
+	return (Account::_nbAccounts);
+}
+
+int Account::getTotalAmount(void)
+{
+	return(Account::_totalAmount);
+}
+
+int Account::getNbDeposits (void)
+{
+	return(Account::_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals (void)
+{
+	return (Account::_totalNbWithdrawals);
+}
+
---
+++ module00/ex02/Account.hpp
@@ -0,0 +1,69 @@
+// ************************************************************************** //
+//                                                                            //
+//                Account.hpp for GlobalBanksters United                //
+//                Created on  : Thu Nov 20 19:43:15 1989                      //
+//                Last update : Wed Jan 04 14:54:06 1992                      //
+//                Made by : Brad "Buddy" McLane <bm@gbu.com>                  //
+//                                                                            //
+// ************************************************************************** //
+
+
+#pragma once
+#ifndef __ACCOUNT_H__
+#define __ACCOUNT_H__
+
+// ************************************************************************** //
+//                               Account Class                                //
+// ************************************************************************** //
+
+class Account {
+
+
+public:
+
+	typedef Account		t;
+
+	static int	getNbAccounts( void ); // ok
+	static int	getTotalAmount( void ); // ok
+	static int	getNbDeposits( void ); // ok
+	static int	getNbWithdrawals( void ); // ok
+	static void	displayAccountsInfos( void ); // ok
+
+	Account( int initial_deposit );
+	~Account( void );
+
+	void	makeDeposit( int deposit );
+	bool	makeWithdrawal( int withdrawal );
+	int		checkAmount( void ) const;
+	void	displayStatus( void ) const;
+
+
+private:
+
+	static int	_nbAccounts; // ok
+	static int	_totalAmount; // ok
+	static int	_totalNbDeposits; // ok
+	static int	_totalNbWithdrawals; // ok
+
+	static void	_displayTimestamp( void );
+
+	int				_accountIndex;
+	int				_amount;
+	int				_nbDeposits;
+	int				_nbWithdrawals;
+
+	Account( void );
+
+};
+
+
+
+// ************************************************************************** //
+// vim: set ts=4 sw=4 tw=80 noexpandtab:                                      //
+// -*- indent-tabs-mode:t;                                                   -*-
+// -*- mode: c++-mode;                                                       -*-
+// -*- fill-column: 75; comment-column: 75;                                  -*-
+// ************************************************************************** //
+
+
+#endif /* __ACCOUNT_H__ */
