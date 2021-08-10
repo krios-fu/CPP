3fa148c77e3ac2d7622bd18420ad6d37775a4641
Kevin Rios fuentes <krios-fu@c1r7s4.42madrid.com>
2021-08-10 21:05:31 +0200

    mod spc

--- module00/ex00/megafone.cpp
+++ module00/ex00/megafone.cpp
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/07 21:59:13 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/08/07 23:16:39 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/08/09 16:42:45 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,7 @@ int	main(int argc, char *argv[])
 	if (argc > 1)
 	{
 		for (int i = 1; i < argc; i++)
-		{
 			megafone(argv[i]);
-			if (i < argc -1)
-				std::cout << " ";
-		}
 	}
 	else
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
