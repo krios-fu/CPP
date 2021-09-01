3e9f09a1eb876410786d4bcbbb51cf236ccc9022
Kevin Rios fuentes <krios-fu@c1r7s4.42madrid.com>
2021-09-01 18:35:13 +0200

    primera entrega module01

--- module01/ex05/Karen.cpp
+++ module01/ex05/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 16:48:34 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/09/01 18:22:50 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/09/01 18:32:47 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,24 +45,20 @@ void Karen::complain( std::string level)
 
 void Karen::debug ( void )
 {
-	std::cout << "[ DEBUG ]" << std::endl;
-	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. \nI just love it!" << std::endl;
 }
 
 void Karen::info ( void )
 {
-	std::cout << "[ INFO ]" << std::endl;
-	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!"<< std::endl;
+	std::cout << "I cannot believe adding extra bacon cost more money. \nYou don’t put enough! If you did I would not have to ask for it!"<< std::endl;
 }
 
 void Karen::warning ( void )
 {
-	std::cout << "[ WARNING ]" << std::endl;
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. \nI’ve been coming here for years and you just started working here last month." << std::endl;
 }
 
 void Karen::error ( void )
 {
-	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
--- module01/ex06/Karen.cpp
+++ module01/ex06/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 16:48:34 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/09/01 18:25:45 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/09/01 18:34:21 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,20 +53,24 @@ void Karen::complain( std::string level)
 
 void Karen::debug ( void )
 {
-	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. \nI just love it!\n" << std::endl;
 }
 
 void Karen::info ( void )
 {
-	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!"<< std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon cost more money. \nYou don’t put enough! If you did I would not have to ask for it!\n"<< std::endl;
 }
 
 void Karen::warning ( void )
 {
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. \nI’ve been coming here for years and you just started working here last month.\n" << std::endl;
 }
 
 void Karen::error ( void )
 {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
