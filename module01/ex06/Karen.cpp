77f8e95ec1117ab800a9ec107209c17c9f215245
Kevin Rios fuentes <krios-fu@c1r7s4.42madrid.com>
2021-09-01 18:26:37 +0200

    module/ex06 ok

--- module01/ex06/Karen.cpp
+++ module01/ex06/Karen.cpp
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/01 16:48:34 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/09/01 18:22:50 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/09/01 18:25:45 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,29 +40,33 @@ void Karen::complain( std::string level)
 
 	for (size_t i = 0; i < 4; i++)
 		if(lvl[i] == level)
-			(this->*fPtr[i])();
+		{
+			while (i < 4)
+			{
+				(this->*fPtr[i])();
+				i++;
+			}
+			return ;
+		}
+	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 }
 
 void Karen::debug ( void )
 {
-	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love to get extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I just love it!" << std::endl;
 }
 
 void Karen::info ( void )
 {
-	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon cost more money. You don’t put enough! If you did I would not have to ask for it!"<< std::endl;
 }
 
 void Karen::warning ( void )
 {
-	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming here for years and you just started working here last month." << std::endl;
 }
 
 void Karen::error ( void )
 {
-	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable, I want to speak to the manager now." << std::endl;
 }
---
+++ module01/ex06/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 17:32:19 by krios-fu          #+#    #+#             */
+/*   Updated: 2021/09/01 18:24:37 by krios-fu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Karen.hpp"
+
+int main (int argc, char *argv[])
+{
+	Karen karencilla;
+
+	if (argc == 2)
+	{
+		karencilla.complain(argv[1]);
+	}
+	else
+		std::cout << "Options levels: INFO, WARNING, DEBUG, ERROR" << std::endl;
+	return (0);
+}
