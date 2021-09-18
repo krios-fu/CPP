a1c7a650a3103022d3da85ec3e556cbb66299c1c
Kevin Rios fuentes <krios-fu@c1r5s1.42madrid.com>
2021-09-18 19:29:52 +0200

    module05 100%

--- module05/ex00/main.cpp
+++ module05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 17:34:10 by krios-fu          #+#    #+#             */
-/*   Updated: 2021/09/12 17:58:52 by krios-fu         ###   ########.fr       */
+/*   Updated: 2021/09/18 15:55:48 by krios-fu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,24 @@ int	main()
 		Bureaucrat a("Kevin", 150);
 		Bureaucrat b;
 
-		b.setGrade(0);
+		b.setGrade(1);
 		std::cout << a << std::endl;
-		
-		// a.decrementGrade();
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
 	
+	try
+	{
+		Bureaucrat b;
+		b.setGrade(0);
+		std::cout << b << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	return 0;
 }
