36b7dc08df892b470a5aea075553522c27c289a3
Kevin Rios fuentes <krios-fu@c1r5s1.42madrid.com>
2021-09-24 03:49:26 +0200

    module08/ex02 ok

---
+++ module08/ex02/MutantStack.hpp
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 01:48:48 by krios-fu          #+#    #+#             */
+/*   Updated: 2021/09/24 03:10:36 by krios-fu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANT_STACK_HPP
+#define MUTANT_STACK_HPP
+
+#include <stack>
+#include <deque>
+
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
+
+		MutantStack() : std::stack<T>(){};
+		MutantStack( const MutantStack<T> & other ) : std::stack<T>(other) {};
+virtual	~MutantStack(){};
+
+	MutantStack & operator=( const MutantStack<T> & other )
+	{
+		if ( *this == &other)
+			return *this;
+		std::stack<T>::operator=(other);
+		return *this;
+	};
+
+	iterator	begin()
+	{
+		return this->c.begin();
+	};
+
+	iterator	end()
+	{
+		return this->c.end();
+	};
+	
+	const_iterator	begin()	const
+	{
+		return this->c.begin();
+	};
+
+	const_iterator	end()	const
+	{
+		return this->c.end();
+	};
+
+	reverse_iterator	rbegin()
+	{
+		return this->c.rbegin();
+	};
+	
+	reverse_iterator	rend()
+	{
+		return this->c.rend();
+	};
+
+	const_reverse_iterator	rbegin()	const
+	{
+		return this->c.rbegin();
+	};
+	
+	const_reverse_iterator	rend()		const
+	{
+		return this->c.rend();
+	};
+};
+
+#endif
---
+++ module08/ex02/main.cpp
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krios-fu <krios-fu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/24 02:15:30 by krios-fu          #+#    #+#             */
+/*   Updated: 2021/09/24 03:46:29 by krios-fu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "MutantStack.hpp"
+# include <iostream>
+# include <list>
+
+int main()
+{
+
+std::cout << "************ mutantstack ***********" << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "Size: " << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	std::cout << "Size: " << mstack.size() << std::endl;
+
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	std::cout << "Display stack" << std::endl;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+
+	std::cout << "Display Reverse stack" << std::endl;
+	
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
+	++rit;
+	--rit;
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+
+	std::cout << "*************** list **************" << std::endl;
+	
+	std::list<int> lisa;
+	lisa.push_back(5);
+	lisa.push_back(17);
+	std::cout << lisa.back() << std::endl;
+	lisa.pop_back();
+	std::cout << "Size: " << lisa.size() << std::endl;
+	lisa.push_back(3);
+	lisa.push_back(5);
+	lisa.push_back(737);
+	//[...]
+	lisa.push_back(0);
+	std::cout << "Size: " << lisa.size() << std::endl;
+
+	std::list<int>::iterator lit = lisa.begin();
+	std::list<int>::iterator lite = lisa.end();
+	++lit;
+	--lit;
+	std::cout << "Display list" << std::endl;
+	while (lit != lite)
+	{
+		std::cout << *lit << std::endl;
+		++lit;
+	}
+	std::list<int> s2(lisa);
+
+	std::cout << "Display Reverse list" << std::endl;
+	
+	std::list<int>::reverse_iterator rlit = lisa.rbegin();
+	std::list<int>::reverse_iterator rlite = lisa.rend();
+	++rlit;
+	--rlit;
+	while (rlit != rlite)
+	{
+		std::cout << *rlit << std::endl;
+		++rlit;
+	}
+	
+	return 0;
+}
